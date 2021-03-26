d4aa3d19273b2dde41cf96fe88546339395b0cea
Konstantin Kruglov <kruglovk@gmail.com>
2021-03-26 23:36:13 +0300

    added help for metrics

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func init() {
 	flag.IntVar(&config.Conf.UDPSrv.Port, "udp-server-port", 8888, "port of UDP server")
 	flag.IntVar(&config.Conf.UDPSrv.ReadBuf, "udp-server-in-buf", 524288, "size the buffer incoming data of UDP server")
 	flag.StringVar(&config.Conf.Parse.CutToFirst, "cut-to-first", "_", "to cut to the first character")
-	flag.StringVar(&config.Conf.Parse.LogFormat, "log-format", "_$host $request_method $scheme $status $request_length $request_time $body_bytes_sent $bytes_sent $upstream_status $upstream_connect_time $upstream_response_time $upstream_header_time $upstream_response_length", "log_format from nginx")
+	flag.StringVar(&config.Conf.Parse.LogFormat, "log-format", "_$host $request_method $scheme $status $request_time $upstream_status $upstream_connect_time $upstream_response_time $upstream_header_time", "log_format from nginx")
 	flag.StringVar(&config.Conf.WebMetricsAddr, "web-metrics-addr", ":9999", "bind to address of metrics server")
 	flag.IntVar(&config.Conf.Parse.Workers, "workers", 5, "workers for process log")
 	flag.Parse()
--- metrics/metrics.go
+++ metrics/metrics.go
@@ -10,38 +10,45 @@ var ParseErrorTotal = prometheus.NewCounter(prometheus.CounterOpts{
 // HTTPRequestTotal ...
 var HTTPRequestTotal = prometheus.NewCounterVec(prometheus.CounterOpts{
 	Name: "nglog_http_requests_total",
+	Help: "request method, usually GET or POST and scheme",
 }, []string{"method", "scheme"})
 
 // StatusTotal ...
 var StatusTotal = prometheus.NewCounterVec(prometheus.CounterOpts{
 	Name: "nglog_status_total",
+	Help: "Response status",
 }, []string{"code", "host"})
 
 // RequestTimeHist ...
 var RequestTimeHist = prometheus.NewHistogramVec(prometheus.HistogramOpts{
 	Name:    "nglog_request_time_hist",
+	Help:    "Request processing time in seconds with a milliseconds resolution; time elapsed between the first bytes were read from the client and the log write after the last bytes were sent to the client",
 	Buckets: []float64{.005, .01, .025, .05, .1, .25, .5, 1, 2.5, 5, 10},
 }, []string{})
 
 // UpstreamStatusTotal ...
 var UpstreamStatusTotal = prometheus.NewCounterVec(prometheus.CounterOpts{
 	Name: "nglog_upstream_status_total",
+	Help: "Keeps status code of the response obtained from the upstream server",
 }, []string{"code", "host"})
 
 // UpstreamConnectTimeHist ...
 var UpstreamConnectTimeHist = prometheus.NewHistogramVec(prometheus.HistogramOpts{
 	Name:    "nglog_upstream_connect_time_hist",
+	Help:    "Keeps time spent on establishing a connection with the upstream server",
 	Buckets: []float64{.005, .01, .025, .05, .1, .25, .5, 1, 2.5, 5, 10},
 }, []string{})
 
 // UpstreamResposeTimeHist ...
 var UpstreamResposeTimeHist = prometheus.NewHistogramVec(prometheus.HistogramOpts{
 	Name:    "nglog_upstream_response_time_hist",
+	Help:    "Keeps time spent on receiving the response from the upstream server",
 	Buckets: []float64{.005, .01, .025, .05, .1, .25, .5, 1, 2.5, 5, 10},
 }, []string{})
 
 // UpstreamHeaderTimeHist ...
 var UpstreamHeaderTimeHist = prometheus.NewHistogramVec(prometheus.HistogramOpts{
 	Name:    "nglog_upstream_header_time_hist",
+	Help:    "Keeps time spent on receiving the response header from the upstream server",
 	Buckets: []float64{.005, .01, .025, .05, .1, .25, .5, 1, 2.5, 5, 10},
 }, []string{})
